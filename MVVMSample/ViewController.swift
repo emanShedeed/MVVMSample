bfe2459d64b7e9d33a2a1b7954181f12e743a744
BSS <eman.shedeed@black-squares.com>
2020-07-23 07:54:31 +0200

    Initial Commit

---
+++ MVVMSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMSample
+//
+//  Created by eman shedeed on 7/23/20.
+//  Copyright © 2020 eman shedeed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
